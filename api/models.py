5995957d25b8c7847cae566afac0f9108fe96a9c
therealazimbek <aaazim04012002@gmail.com>
2022-06-15 02:49:50 +0600

    slight changes on models

--- api/models.py
+++ api/models.py
@@ -6,6 +6,9 @@ class Employee(models.Model):
     middlename = models.CharField(max_length=200, blank=False)
     lastname = models.CharField(max_length=200, blank=False)
     position = models.CharField(max_length=200, blank=False)
-    joined_date = models.DateTimeField(auto_now_add=True)
+    joined_date = models.DateField()
     salary = models.PositiveIntegerField(blank=False)
-    supervisor = models.ForeignKey('self', on_delete=models.CASCADE)
+    supervisor = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
